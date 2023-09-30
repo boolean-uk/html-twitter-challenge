0368ba16fd4d917a3c0f3ed1074b666a045bd795
Tayokanch <tayoquadri11@gmail.com>
2023-09-30 09:50:30 +0100

    lastest commit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-
-
 const nav = document.getElementById('nav');
 const navIcon = document.getElementsByClassName('nav-icons');
 const navIconText = document.getElementsByClassName('nav-icon-text');
 const twitter = document.getElementsByClassName('fa-twitter')[0];
 const home = document.getElementsByClassName('fa-house-user')[0];
 const comment = document.getElementById('comment')
-const retweet = document.getElementById('retweet')
 const heart = document.getElementById('heart')
 const uparrow = document.getElementById('uparrow')
 const caution = document.getElementById('caution')
 const navBar = document.getElementById('navbar')
 const container = document.getElementById('container')
+const everyIcons =document.getElementsByTagName('i')
+
+
 
 
 
@@ -30,50 +30,117 @@ for (let i = 0; i < navIcon.length; i++) {
     });
 }
 
- comment.addEventListener( 'click', function() {
-    comment.style.color ='cornflowerblue'
- 
-}) 
+
+const clickUparrowIcon = () =>{
+    let clickUparrow = 'active'
+    uparrow.addEventListener('click', function(){
+        if( clickUparrow  === 'active'){
+            uparrow.style.color = 'green'
+            clickUparrow  = 'deactive'
+        }
+        else{
+            uparrow.style.color = ''
+            clickUparrow = 'active'
+        }
+    })
+
+}
+clickUparrowIcon();
 
 
-heart.addEventListener( 'click', function() {
-    heart.style.color ='red'
- 
-}) 
+const clickCommentIcon = () =>{
+    let clickcomment = 'active'
 
+    comment.addEventListener('click', function(){
+        if( clickcomment === 'active'){
+            comment.style.color = 'rgb(22, 120, 218)'
+            clickcomment = 'deactive'
+        }
+        else{
+            comment.style.color = ''
+            clickcomment = 'active'
+        }
+    })
 
+}
+clickCommentIcon();
 
-caution.addEventListener( 'click', function() {
-    caution.style.color ='red'
- 
-}) 
-
-retweet.addEventListener( 'click', function() {
-    retweet.style.color ='green'
- 
-}) 
-
-
-
-let navExpanded = false
-navBar.addEventListener('click', function() {
-   
-    if (navExpanded) {
-        container.style.gridTemplateColumns = '1fr 2fr 1fr';
-    } else {
-        container.style.gridTemplateColumns = '100px auto 1fr';
-    }
-  
-        navExpanded = !navExpanded;
-       
-
-        for (let i = 0; i < navIconText.length; i++) {
-            if (navIconText[i].style.display === 'block' || navIconText[i].style.display === '') {
-                navIconText[i].style.display = 'none'; 
-            } else {
-                navIconText[i].style.display = 'block';
-            }
+const clickHeartIcon = () =>{
+    let clickHeart = 'active'
+    heart.addEventListener('click', function(){
+        if( clickHeart === 'active'){
+            heart.style.color = 'red'
+            clickHeart = 'deactive'
         }
 
-});
- 
+        else{
+            heart.style.color = ''
+            clickHeart = 'active'
+        }
+    })
+    
+}
+clickHeartIcon();
+
+
+
+const clickCaution = () =>{
+    let clickCautionIcon = 'display'
+    caution.addEventListener( 'click', function() {
+
+        if(clickCautionIcon === 'display'){
+            caution.style.color ='red'
+            clickCautionIcon = 'undisplayed'
+        }
+        else{
+            caution.style.color = ''
+            clickCautionIcon = 'display'
+        }
+    }) 
+
+}
+clickCaution();
+
+
+const clickRetweet = () =>{
+    let clickRetweetIcon = 'display'
+    retweet.addEventListener( 'click', function() {
+
+        if(clickRetweetIcon === 'display'){
+            retweet.style.color ='green'
+            clickRetweetIcon = 'undisplayed'
+        }
+        else{
+            retweet.style.color = ''
+            clickRetweetIcon = 'display'
+        }
+    }) 
+
+    
+
+}
+clickRetweet();
+
+
+
+let clicked = 'closed'
+navBar.addEventListener('click', function(){
+    const retweet = document.getElementById('retweet')
+    for (let i = 0; i < navIconText.length; i++) {
+        if (navIconText[i].style.display === 'block' || navIconText[i].style.display === '') {
+            navIconText[i].style.display = 'none'; 
+        } else {
+            navIconText[i].style.display = 'block';
+        }
+
+    }
+
+    if(clicked === 'closed'){
+        container.style.gridTemplateColumns = '100px 2fr 1fr';
+        clicked = 'opened'
+    }
+    else{
+        container.style.gridTemplateColumns = 'auto 2fr 1fr';
+        clicked = 'closed'
+    }
+})
